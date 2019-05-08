da71d2501481a90da8a2d6faff37686a93eaba21
akhilnbr <mcakhil@gmail.com>
2019-05-08 22:54:32 +0530

    Add project files.

---
+++ Employee_Attendance_Tracker_System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Employee_Attendance_Tracker_System.MvcApplication" Language="C#" %>
---
+++ Employee_Attendance_Tracker_System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Employee_Attendance_Tracker_System.Startup))]
+namespace Employee_Attendance_Tracker_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
